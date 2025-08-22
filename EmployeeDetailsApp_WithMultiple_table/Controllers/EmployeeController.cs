aba209d7a12921514872ab044206b10246caf208
naimreza <78632945+naimrezap@users.noreply.github.com>
2025-08-22 16:30:29 +0600

    JoinModel Added

--- EmployeeDetailsApp_WithMultiple_table/Controllers/EmployeeController.cs
+++ EmployeeDetailsApp_WithMultiple_table/Controllers/EmployeeController.cs
@@ -14,13 +14,30 @@ namespace EmployeeDetailsApp_WithMultiple_table.Controllers
         }
         public IActionResult Index()
         {
-              //*******This for Model Merging**********
+            //*******This for Model Merging**********
 
-            ViewModel emp = new ViewModel();
+            // ViewModel emp = new ViewModel();
             //var empData = _context.Employees.ToList();
             //var depData = _context.Departments.ToList();
             //emp.Employees = empData;
             //emp.Departments = depData;
+
+            //*******This for Join Model **********
+            var emp = (from e in _context.Employees
+                       join d in _context.Departments
+                       on e.DepartmentId equals d.DepartmentId
+                       select new JoinModel
+                       {
+                           EmployeeId = e.EmployeeId,
+                           FirstName = e.FirstName,
+                           LastName = e.LastName,
+                           Gender = e.Gender,
+                           Email = e.Email,
+                           DepartmentName = d.DepartmentName,
+                           DepartmentCode = d.DepartmentCode
+
+                       }).ToList();
+
             return View(emp);
         }
     }
---
+++ EmployeeDetailsApp_WithMultiple_table/Models/ViewModel/JoinModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeDetailsApp_WithMultiple_table.Models.ViewModel
+{
+    public class JoinModel
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Gender { get; set; }
+        public string? Email { get; set; }
+        public string DepartmentName { get; set; }
+        public string DepartmentCode { get; set; }
+    }
+}
