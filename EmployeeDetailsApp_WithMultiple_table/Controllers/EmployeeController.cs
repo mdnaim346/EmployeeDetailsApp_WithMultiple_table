b136af795cdbcc0f979b460d9757a636b3f724d2
naimreza <78632945+naimrezap@users.noreply.github.com>
2025-08-22 16:09:52 +0600

    ModelMerging

--- EmployeeDetailsApp_WithMultiple_table/Controllers/EmployeeController.cs
+++ EmployeeDetailsApp_WithMultiple_table/Controllers/EmployeeController.cs
@@ -1,5 +1,7 @@
 ﻿using EmployeeDetailsApp_WithMultiple_table.Data;
+using EmployeeDetailsApp_WithMultiple_table.Models.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 
 namespace EmployeeDetailsApp_WithMultiple_table.Controllers
 {
@@ -12,8 +14,14 @@ namespace EmployeeDetailsApp_WithMultiple_table.Controllers
         }
         public IActionResult Index()
         {
-            var empData = _context.Employees.ToList();
-            return View(empData);
+              //*******This for Model Merging**********
+
+            ViewModel emp = new ViewModel();
+            //var empData = _context.Employees.ToList();
+            //var depData = _context.Departments.ToList();
+            //emp.Employees = empData;
+            //emp.Departments = depData;
+            return View(emp);
         }
     }
 }
---
+++ EmployeeDetailsApp_WithMultiple_table/Models/ViewModel/ViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeDetailsApp_WithMultiple_table.Models.ViewModel
+{
+    public class ViewModel
+    {
+        public List<Employee> Employees { get; set; }
+        public  List<Department> Departments { get; set; }
+    }
+}
